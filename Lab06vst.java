74179e887786d750481565560edc72f131a7d210
zach <zachary.schwerdtfeger@vtsd.com>
2022-11-16 11:01:18 -0500

    uhh

--- Lab06vst.java
+++ Lab06vst.java
@@ -22,24 +22,43 @@ public class Lab06vst extends Applet {
         Random rand = new Random();
 
         for (int k = 0; k <= 100; k++) {
-            System.out.println(rand.nextInt(390)+10);
-            int x1 = rand.nextInt(390);
-            int y1 = rand.nextInt(290);
-            int x2 = rand.nextInt(390);
-            int y2 = rand.nextInt(290);
 
-            g.drawLine(x1, y1, x2,y2);
+            int x1 = rand.nextInt(391) + 10;
+            int y1 = rand.nextInt(291) + 10;
+            int x2 = rand.nextInt(391) + 10;
+            int y2 = rand.nextInt(291) + 10;
+
+            int red = rand.nextInt(255);
+            int green = rand.nextInt(255);
+            int blue = rand.nextInt(255);
+
+            g.setColor(new Color(red, green, blue));
+            g.drawLine(x1, y1, x2, y2);
 
             // Draw Random Squares
+        }
+            for (int b = 0; b <= 100; b++) {
 
-            // Draw Random Circles
+                int b1 = rand.nextInt(381) + 400;
+                int c1 = rand.nextInt(281) + 10;
 
 
-            // Draw 3-D Box
+                int red = rand.nextInt(255);
+                int green = rand.nextInt(255);
+                int blue = rand.nextInt(255);
+
+                g.setColor(new Color(red,green,blue));
+                g.fillRect(b1, c1, 50, 50);
+
+                // Draw Random Circles
+
+
+                // Draw 3-D Box
+
+
 
 
         }
-
     }
 }
 
